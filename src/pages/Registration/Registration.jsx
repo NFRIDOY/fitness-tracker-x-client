062c20645ac739a75b8a3656b18a39a22146cc32
NF RIDOY <nfridoy@gmail.com>
2023-11-25 00:46:05 +0600

    Fix: Regi dark mode

--- src/pages/Registration/Registration.jsx
+++ src/pages/Registration/Registration.jsx
@@ -67,13 +67,13 @@ export default function Registration() {
 
             <section className="bg-white w-full ">
                 <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-                    <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+                    <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-white">
                         {/* <img className="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo"/>
                               Flowbite */}
                     </a>
-                    <div className="w-full  rounded-lg shadow-2xl  dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                    <div className="w-full  rounded-lg shadow-2xl  border md:mt-0 sm:max-w-md xl:p-0 bg-gray-800 border-gray-700">
                         <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-                            <h1 className="text-center text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                            <h1 className="text-center text-xl font-bold leading-tight tracking-tight  md:text-2xl text-white">
                                 Registration for your account
                             </h1>
                             <form className="space-y-4 md:space-y-8 h-80" onSubmit={handleReg}>
@@ -81,23 +81,23 @@ export default function Registration() {
                                 <div className="flex space-x-4">
                                     <div className="space-y-8">
                                         <div >
-                                            <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                                            <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary focus:border-primary block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@mail.com" required />
+                                            <label htmlFor="email" className="block mb-2 text-sm font-medium text-white">Your email</label>
+                                            <input type="email" name="email" id="email" className="bg-gray-50 border border-gray  sm:text-sm rounded-lg   block w-full p-2.5 dark:bg-gray-700 border-gray-600 dark:placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500" placeholder="name@mail.com" required />
                                         </div>
                                         <div>
                                             <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                                            <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary focus:border-primary block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                                            <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg  block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white focus:ring-blue-500 focus:border-blue-500" required />
                                         </div>
                                     </div>
                                     <div className="space-y-8">
                                         <div>
                                             <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your name</label>
-                                            <input type="text" name="name" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary focus:border-primary block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name" required />
+                                            <input type="text" name="name" id="name" className="bg-gray-50 border border-gray-300 sm:text-sm rounded-lg   block w-full p-2.5 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500" placeholder="name" required />
                                         </div>
 
                                         <div>
-                                            <label htmlFor="photoURL" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Photo URL</label>
-                                            <input type="text" name="photoURL" id="photoURL" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary focus:border-primary block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="URL" required />
+                                            <label htmlFor="photoURL" className="block mb-2 text-sm font-medium  text-white">Photo URL</label>
+                                            <input type="text" name="photoURL" id="photoURL" className="bg-gray-50 border border-gray-300  sm:text-sm rounded-lg  focus:border-primary block w-full p-2.5 600 placeholder-gray-400 text-white focus:ring-blue-500 dark:focus:border-blue-500" placeholder="URL" required />
                                         </div>
                                     </div>
                                 </div>
@@ -112,10 +112,10 @@ export default function Registration() {
                                     </div>
                                     {/* <a href="#" className="text-sm font-medium text-primary hover:underline dark:text-primary">Forgot password?</a> */}
                                 </div>
-                                <button type="submit" className="w-full text-white bg-primary hover:bg-primary focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary dark:hover:bg-primary dark:focus:ring-primary-800">Registration</button>
+                                <button type="submit" className="w-full text-white focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-primary hover:bg-primary focus:ring-primary-800">Registration</button>
 
-                                <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                                    Have an account? <Link to={"/Login"} href="#" className="font-medium text-primary hover:underline dark:text-white">Log In</Link>
+                                <p className="text-sm font-light text-gray-400">
+                                    Have an account? <Link to={"/Login"} href="#" className="font-medium hover:underline text-white">Log In</Link>
                                 </p>
                             </form>
                         </div>
