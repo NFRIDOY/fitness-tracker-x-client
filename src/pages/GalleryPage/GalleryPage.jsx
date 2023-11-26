96e914bc951647e07695bd80bfef93b4867b2a7c
NF RIDOY <nfridoy@gmail.com>
2023-11-27 03:33:07 +0600

    Fix: Gallery caption

--- src/pages/GalleryPage/GalleryPage.jsx
+++ src/pages/GalleryPage/GalleryPage.jsx
@@ -10,7 +10,7 @@ export default function GalleryPage() {
             width: 320,
             height: 174,
             // isSelected: true,
-            caption: "",
+            // caption: "",
         },
         {
             src: "https://plus.unsplash.com/premium_photo-1670513670485-b23274483f89?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
@@ -20,7 +20,7 @@ export default function GalleryPage() {
             //     { value: "Ocean", title: "Ocean" },
             //     { value: "People", title: "People" },
             // ],
-            alt: "Boats (Jeshu John - designerspics.com)",
+            // alt: "Boats (Jeshu John - designerspics.com)",
         },
         {
             src: "https://images.unsplash.com/photo-1605296867304-46d5465a13f1?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
@@ -32,17 +32,17 @@ export default function GalleryPage() {
             width: 320,
             height: 174,
             // isSelected: true,
-            caption: "After Rain (Jeshu John - designerspics.com)",
+            // caption: "After Rain (Jeshu John - designerspics.com)",
         },
         {
             src: "https://images.unsplash.com/photo-1594882645126-14020914d58d?q=80&w=1785&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
             width: 320,
             height: 212,
-            tags: [
-                { value: "Ocean", title: "Ocean" },
-                { value: "People", title: "People" },
-            ],
-            alt: "Boats (Jeshu John - designerspics.com)",
+            // tags: [
+            //     { value: "Ocean", title: "Ocean" },
+            //     { value: "People", title: "People" },
+            // ],
+            // alt: "Boats (Jeshu John - designerspics.com)",
         },
         {
             src: "https://images.unsplash.com/photo-1517838277536-f5f99be501cd?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
@@ -53,17 +53,17 @@ export default function GalleryPage() {
             width: 320,
             height: 174,
             // isSelected: true,
-            caption: "After Rain (Jeshu John - designerspics.com)",
+            // caption: "After Rain (Jeshu John - designerspics.com)",
         },
         {
             src: "https://images.unsplash.com/photo-1571902943202-507ec2618e8f?q=80&w=1975&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
             width: 320,
             height: 212,
-            tags: [
-                { value: "Ocean", title: "Ocean" },
-                { value: "People", title: "People" },
-            ],
-            alt: "Boats (Jeshu John - designerspics.com)",
+            // tags: [
+            //     { value: "Ocean", title: "Ocean" },
+            //     { value: "People", title: "People" },
+            // ],
+            // alt: "Boats (Jeshu John - designerspics.com)",
         },
         {
             src: "https://images.unsplash.com/photo-1483721310020-03333e577078?q=80&w=1856&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
@@ -75,17 +75,17 @@ export default function GalleryPage() {
             width: 320,
             height: 174,
             // isSelected: true,
-            caption: "After Rain (Jeshu John - designerspics.com)",
+            // caption: "After Rain (Jeshu John - designerspics.com)",
         },
         {
             src: "https://images.unsplash.com/photo-1584735935682-2f2b69dff9d2?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
             width: 320,
             height: 212,
-            tags: [
-                { value: "Ocean", title: "Ocean" },
-                { value: "People", title: "People" },
-            ],
-            alt: "Boats (Jeshu John - designerspics.com)",
+            // tags: [
+            //     { value: "Ocean", title: "Ocean" },
+            //     { value: "People", title: "People" },
+            // ],
+            // alt: "Boats (Jeshu John - designerspics.com)",
         },
         {
             src: "https://images.unsplash.com/photo-1507398941214-572c25f4b1dc?q=80&w=1973&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
