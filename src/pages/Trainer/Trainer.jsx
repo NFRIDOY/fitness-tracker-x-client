98f090b1ce401657469c450e49f4d31c8cf387a2
NF RIDOY <nfridoy@gmail.com>
2023-11-27 03:31:41 +0600

    Show all Trainers on triner Page

--- src/pages/Trainer/Trainer.jsx
+++ src/pages/Trainer/Trainer.jsx
@@ -8,6 +8,8 @@ import { useEffect, useState } from 'react';
 import useAuth from '../../hook/useAuth';
 import { useQuery } from '@tanstack/react-query';
 import LoadingAnimations from '../../components/LoadingAnimations/LoadingAnimations';
+import { FaFacebook } from "react-icons/fa";
+import Container from './../../components/Container/Container';
 
 export default function Trainer() {
 
@@ -22,7 +24,7 @@ export default function Trainer() {
             axios.get(`/trainers`).then(
                 (res) => {
                     console.log(res.data)
-                    console.log(alltrainers)
+                    // console.log(alltrainers)
                     setTrainers(res.data)
                 },
             ),
@@ -45,15 +47,60 @@ export default function Trainer() {
             </Header1>
             {/* All Trainers */}
             <section>
-                <h1>
+                <h1 className='text-center text-xl underline italic'> Total Trainers : 
                     {
                         trainers.length
                     }
                 </h1>
+                {/* <div className="card w-96 bg-base-100 shadow-xl">
+                    <div className="card-body">
+                        <h2 className="card-title">T</h2>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                        <div className="card-actions justify-end">
+                            <button className="btn btn-primary">Buy Now</button>
+                        </div>
+                    </div>
+                </div> */}
+                <Container>
+                    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-full'>
+                        {
+                            trainers.map(({ _id, email,
+                                fullName,
+                                age,
+                                description,
+                                skills,
+                                week,
+                                day,
+                                fbLink,
+                                experience,
+                                role,
+                                status }) => {
+                                return <div className="card w-96 bg-base-100 shadow-xl" key={_id}>
+                                    <div className="card-body">
+                                        <h2 className="card-title">{fullName}</h2>
+                                        <p>{description}</p>
+                                        <div className='flex'>
+                                            <p>{week}</p>
+                                            <p>{day}</p>
+                                        </div>
+                                        <p>{skills}</p>
+                                        <p>Years Of Expresience: {experience} Years</p>
+                                        <p>{role}</p>
+                                        <p><Link to={fbLink} ><FaFacebook /></Link></p>
+                                        <div className="card-actions justify-end">
+                                            <button className="btn btn-primary text-white"> Know More</button>
+                                        </div>
+                                    </div>
+                                </div>
+                                // console.log(trainer)
+                            })
+                        }
+                    </div>
+                </Container>
             </section>
             {/* be a Trainer */}
-            <section>
-                <Link to={"/BeATrainer"} className='btn btn-success text-white'>
+            <section className='mx-auto w-fit'>
+                <Link to={"/BeATrainer"} className='btn btn-success text-white '>
                     Be A Trainer
                 </Link>
                 {/* <PrivateRoute>
